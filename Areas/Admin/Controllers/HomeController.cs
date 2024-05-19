b17b5a7753c3dc9dd07d7836f984d422736daf3a
Ehsan <167895549+EhsanRezapour1387@users.noreply.github.com>
2024-05-20 00:47:04 +0330

    .netproject

--- Areas/Admin/Controllers/HomeController.cs
+++ Areas/Admin/Controllers/HomeController.cs
@@ -4,8 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 using projectdotnet.Models;
 
 namespace projectdotnet.Areas.Admin.Controllers;
-[Area("Admin")]
 [Authorize]
+[Area("Admin")]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
